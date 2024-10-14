62a6db6066aa2fca56bea3719bdd4b2da8b18fdd
Natali <ns@numbuster.com>
2024-10-14 22:19:29 +0300

    Initial Commit

---
+++ SuffixArrayExample/SuffixArrayExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuffixArrayExampleApp.swift
+//  SuffixArrayExample
+//
+//  Created by Natalia Sinitsyna on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuffixArrayExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
